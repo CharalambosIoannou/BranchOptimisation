4784214dffb36649d164170022846412dedccd3c
Charalambos Ioannou <45453951+CharalambosIoannou@users.noreply.github.com>
2022-07-13 14:01:50 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Branch Optimisation'
+copyright = '2022, KPMG'
+author = 'KPMG'
 
 release = '0.1'
 version = '0.1.0'
